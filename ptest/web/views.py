6b02716a5b829a512ffdc59b05d66eaa3b5b4f5b
Andrey <andreyrmg@gmail.com>
2014-11-27 19:14:32 +0300

    обработка ошибок

--- ptest/web/views.py
+++ ptest/web/views.py
@@ -106,3 +106,13 @@ def submit():
             flash(('success', 'success:', 'solution sent!'))
             return redirect(url_for('submissions', user=user))
     return render_template('submit.html', title='Submit', error=error)
+
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html'), 500
